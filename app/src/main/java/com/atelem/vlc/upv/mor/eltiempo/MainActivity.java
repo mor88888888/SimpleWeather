608603059569acdf729edaa63b9ef3959a1d5afb
Ferran <ferran6_95@hotmail.com>
2018-06-19 00:21:30 +0200

    xd

--- app/src/main/java/com/atelem/vlc/upv/mor/eltiempo/MainActivity.java
+++ app/src/main/java/com/atelem/vlc/upv/mor/eltiempo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.atelem.vlc.upv.mor.eltiempo;
 
+import java.sql.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String [] Consulta (String Ciudad){
+        private EditText Poblacion = (EditText) findViewById(R.id/editText);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Error abriendo el driver: " +e.toString());
+            return;
+        }
+
+            //INICIALIZACIONES Y DEFINICIONES
+            Connection con;
+            Statement st;
+            ResultSet rs;
+            String SQL;
+
+
+            //CONEXIONES
+            con=DriverManager.getConnection("jdbc:mysql://localhost/despach","root","");
+
+            st = con.createStatement();
+
+            out = res.getWriter();
+
+
+
+    }
+    //Private string Poblacion;
 }
