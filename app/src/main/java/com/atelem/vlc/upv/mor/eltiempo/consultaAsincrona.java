3ac2e2533837fd3850b2ad4077105915d95a43a3
mor8 <jomoal6@teleco.upv.es>
2018-06-20 00:35:46 +0200

    Pruebas

--- app/src/main/java/com/atelem/vlc/upv/mor/eltiempo/consultaAsincrona.java
+++ app/src/main/java/com/atelem/vlc/upv/mor/eltiempo/consultaAsincrona.java
@@ -3,8 +3,12 @@ package com.atelem.vlc.upv.mor.eltiempo;
 import android.os.AsyncTask;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import okhttp3.*;
+import javax.json.*;
 
 public class consultaAsincrona extends AsyncTask<String, Void, Boolean>{
 
@@ -12,7 +16,6 @@ public class consultaAsincrona extends AsyncTask<String, Void, Boolean>{
 
     protected Boolean doInBackground(String... NOMBRE){
         try {
-
             System.out.println("Nombre: "+NOMBRE[0]);
             //URL url = new URL("http://servermor.asuscomm.com:8564/bd.php?nombre=" + NOMBRE[0]);
             URL url = new URL("http://192.168.1.12/bd.php?nombre=" + NOMBRE[0]);
@@ -25,7 +28,21 @@ public class consultaAsincrona extends AsyncTask<String, Void, Boolean>{
         } catch (Exception e){
             System.out.println(e);
         }
-        return null;
+
+        try {
+            String api_key="";
+            URL url = new URL("https://opendata.aemet.es/opendata/api/"
+                    +"prediccion/especifica/municipio/diaria/"
+                    +inputLine+"?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb21vYWw2QGRvY3Rvci51cHYuZXMiLCJqdGkiOiJjMjc3YmIzZS04YWEwLTQ4MGMtOTFmZC1hODNlZjYxMjdkMTciLCJpc3MiOiJBRU1FVCIsImlhdCI6MTUyNjAyNDE1NywidXNlcklkIjoiYzI3N2JiM2UtOGFhMC00ODBjLTkxZmQtYTgzZWY2MTI3ZDE3Iiwicm9sZSI6IiJ9.wa5Ft9x1FyKTJKRxP-DcO3urDFGNp6zSnFoAdb33Cc8");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String ok="";
+            while((ok = in.readLine()) != null){
+                System.out.println(ok);
+            }
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        return true;
     }
 
 
