f3c20fd1ae0b9e4099ddb8e5abe594c0a33f8d8a
Ferran <ferran6_95@hotmail.com>
2018-06-19 20:25:41 +0200

    xd

--- app/src/main/java/com/atelem/vlc/upv/mor/eltiempo/MainActivity.java
+++ app/src/main/java/com/atelem/vlc/upv/mor/eltiempo/MainActivity.java
@@ -8,6 +8,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText Poblacion;
@@ -15,54 +28,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Poblacion = (EditText)findViewById(R.id.editText);
     }
 
-    public Array [] Consulta (View view){
-        //new Thread(new Runnable() {
-           // public void run() {
+    public void Consulta (View view) {
+        try {
+            String NOMBRE = Poblacion.getText().toString();
+            System.out.println(NOMBRE);
+            URL url = new URL("http://servermor.asuscomm.com?nombre=" + NOMBRE);
+            String inputLine;
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            while((inputLine = in.readLine()) != null){
+                System.out.println(inputLine);
+            }
+        } catch (Exception e){
+            System.out.println(e);
+        }
 
-                Poblacion = findViewById(R.id.editText);
-                Log.d("busqueda", String.valueOf(Poblacion));
-                String driver = "com.mysql.jdbc.Driver";
-
-                try {
-                    System.out.println("hola1");
-                    /*Cargamos el driver del conector JDBC.*/
-                    Class.forName(driver).newInstance();
-                } catch (Exception e) {
-
-                    System.out.println("Error abriendo el driver: " + e.toString());
-                }
-
-                //INICIALIZACIONES Y DEFINICIONES
-                Connection con;
-                Statement st;
-                ResultSet rs;
-                String SQL = "SELECT CPRO, CMUN, NOMBRE FROM Codigo Where='" + Poblacion + "'";
-                Array[] out = new Array[20];
-
-
-                //CONEXIONES
-                try {
-
-                    con = DriverManager.getConnection("jdbc:mysql://servermor.asuscomm.com:1562/eltiempo", "root", "mor");
-                    System.out.println("hola2");
-                    st = con.createStatement();
-                    System.out.println("hola3");
-                    rs = st.executeQuery(SQL);
-                    for (int i = 0; rs.next(); i++)
-
-                    {
-                        out[i] = rs.getArray(i);
-                    }
-
-                } catch (SQLException e) {
-                    System.out.println(e);
-                }
-        //    }
-
-        //}).start();
-
-        return out;
     }
+
 }
